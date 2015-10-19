275cce0d087521b24fa3d35a52d33a30eaa2ff0a
Joshua Johanan <josh359@gmail.com>
2015-10-19 00:40:30 -0400

    a lot of little fixes in Ansible.

---
+++ ansible/roles/new_wordpress/templates/Dockerfile.j2
@@ -0,0 +1,5 @@
+FROM debian:jessie
+
+COPY '{{ site_name }}' '/var/www/html/{{ site_name }}'
+
+VOLUME /var/www/html/
--- production_build.sh
+++ production_build.sh
@@ -3,4 +3,4 @@ source ./venv/bin/activate
 source ./.env
 
 ansible-playbook -i $1, ./ansible/server_setup.yml
-ansible-playbook -i $1, ./ansible/load_wordpress_setup.yml --extra-vars "site_name=$1"
+ansible-playbook -i $1, ./ansible/new_wordpress_setup.yml --extra-vars "site_name=$1 wordpress_version=$2"
